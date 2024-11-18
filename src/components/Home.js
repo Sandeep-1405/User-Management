dcb599fde15616a15564de3e4a7fb5c8c7db9839
Sandeep-1405 <sandeepmaraboina14s@gmail.com>
2024-11-18 22:18:15 +0530

    json data updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,10 @@ const Home = () => {
 
     useEffect(()=>{
         const fetchUsers = ()=>{
-            axios.get('http://localhost:3000/users')
+            axios.get('https://my-json-server.typicode.com/Sandeep-1405/User-Management/users')
             .then(res=>{
-                console.log(res)
+                console.log(res.data)
+                setDisplayData(res.data)
             })
             .catch(error=>{
                 console.log(error)
@@ -18,9 +19,31 @@ const Home = () => {
         fetchUsers();
     },[])
 
+    const emptyList =()=>{
+        return(
+            <div><h3>No User Found</h3></div>
+        )
+    }
+
+    const displayUser = () =>{
+        return(
+            <div>
+                {displayData.map(user=>(
+                    <div key={user.id}>
+                        <h3>{user.firstName}</h3>
+                        <h3>{user.lastName}</h3>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+
     return (
         <div>
             <h1>Users</h1>
+            {
+                displayData ? displayUser() : emptyList()
+            }
         </div>
     );
 };
